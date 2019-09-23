3ea661c4a2a13320d8b9cf79995774d1ec460449
PAL-7 <work.ranjeet@gmail.com>
2019-09-23 11:48:55 +0530

    Simple .NET Core Web API app

---
+++ src/PalTracker/Controllers/WelcomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PalTracker
+{
+    [Route("/")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public string SayHello() => "hello";
+    }
+}
