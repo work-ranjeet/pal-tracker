80db409d22f98009a1cf6339536d46503c0bafdd
PAL-7 <work.ranjeet@gmail.com>
2019-09-24 15:27:30 +0530

    Database

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -32,7 +33,8 @@ namespace PalTracker
                 Configuration.GetValue<string>("MemoryLimit", "MemoryLimit not configured."),
                 Configuration.GetValue<string>("CfInstanceIndex", "CfInstanceIndex not configured."),
                 Configuration.GetValue<string>("CfInstanceAddr", "CfInstanceAddr not configured.")));
-                services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
